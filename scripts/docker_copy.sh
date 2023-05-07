8f89e27cf293a5d081684fe4575811ee12e65c70
Damian Kolaska <damian.kolaska99@gmail.com>
2023-05-07 09:23:43 +0200

    Add first model + test_model script

--- scripts/docker_copy.sh
+++ scripts/docker_copy.sh
@@ -4,4 +4,5 @@ docker cp data/routes.csv siu:/root/
 docker cp solutions/turtlesim_env_base.py siu:/root/
 docker cp solutions/turtlesim_env_single.py siu:/root/
 docker cp solutions/dqn_single.py siu:/root/
+docker cp solutions/test_model.py siu:/root/
 docker cp scripts/docker_setup.sh siu:/root/
---
+++ solutions/test_model.py
@@ -0,0 +1,24 @@
+import numpy as np
+from keras.models import load_model
+
+import turtlesim_env_single
+from dqn_single import DqnSingle
+
+
+# przykładowe wywołanie uczenia
+if __name__ == "__main__":
+    env=turtlesim_env_single.provide_env()                      # utworzenie środowiska
+    env.setup('routes.csv',agent_cnt=1)                         # połączenie z symulatorem
+    env.SPEED_FINE_RATE = -5.0                                  # zmiana wybranych parametrów środowiska
+    agents=env.reset()                                          # ustawienie agenta
+    tname=list(agents.keys())[0]                                # 'lista agentów' do wytrenowania
+    dqns=DqnSingle(env)                                         # utworzenie klasy uczącej
+    dqns.model=load_model(f"models/dqns_model_450.tf")           # albo załadowanie zapisanej wcześniej
+
+    current_state=agents[tname].map
+    last_state=[i.copy() for i in current_state]                            # zaczyna od postoju: poprz. stan taki jak obecny
+    while True:
+        control=np.argmax(dqns.decision(dqns.model,last_state,current_state))
+        new_state,reward,done=env.step({tname:dqns.ctl2act(control)})  #krok symulacji
+        last_state = current_state
+        current_state = new_state
