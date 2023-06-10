24ac14cf03bf4ef86f37ac7fcaeed12a3adb03bb
Damian Kolaska <damian.kolaska99@gmail.com>
2023-06-10 16:32:39 +0200

    New routes

--- scripts/docker_copy.sh
+++ scripts/docker_copy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-docker cp data/roads.png siu:/
-docker cp data/routes.csv siu:/root/
+docker cp data/roads2.png siu:/roads.png
+docker cp data/routes2.csv siu:/root/routes.csv
 docker cp solutions/turtlesim_env_base.py siu:/root/
 docker cp solutions/turtlesim_env_single.py siu:/root/
 docker cp solutions/dqn_single.py siu:/root/
 docker cp solutions/test_model.py siu:/root/
 docker cp scripts/docker_setup.sh siu:/root/
+docker cp solutions/dqn_multi.py siu:/root/
+docker cp solutions/turtlesim_env_multi.py siu:/root/
+docker cp solutions/play-dqn.py siu:/root/
